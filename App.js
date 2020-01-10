54bdecce4088216358124a6fbe0e21f4a3a79f33
Stuart Dambi <stuartdambi@gmail.com>
2020-01-10 11:34:35 +0300

    added AdMobBanner to the app, though still having render issues

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import AdMobBanner from "expo";
 
 export default class App extends React.Component {
+  bannerError(e) {
+    alert(e);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{color: "white"}}>Hello Static World, this is a Google banner</Text>
+        <View>
+          <AdMobBanner
+            bannerSize="fullBanner"
+            adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+            testDeviceID="EMULATOR"
+            servePersonalizedAds // true or false
+            onDidFailToReceiveAdWithError={this.bannerError}
+          />
+        </View>
       </View>
     );
   }
